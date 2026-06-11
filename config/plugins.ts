12ea03f180254809e3de5409a8b0e6c6f449c023
julianbrasse-zhpf <julian.brasse@zhpf.ch>
2026-06-11 18:02:09 +0000

    Modify configuration so render build succeeds

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,11 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+	'users-permissions': {
+		config: {
+			jwtSecret: env('JWT_SECRET'),
+		},
+	},
+});
 
 export default config;
